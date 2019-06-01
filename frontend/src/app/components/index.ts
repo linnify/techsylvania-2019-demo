b77f9eafbefbfcb0f1c54414c8fd388721f328a3
razvanbretoiu <razvan.bretoiu@linnify.com>
2019-06-01 19:02:38 +0300

    feat(web): upload data to google cloud storage

---
+++ frontend/src/app/components/index.ts
@@ -0,0 +1,13 @@
+import { AddDataButtonComponent } from './add-data-button/add-data-button.component';
+import { AppBarComponent } from './app-bar/app-bar.component';
+import { AddDataComponent } from './add-data/add-data.component';
+
+export const components = [
+  AddDataButtonComponent,
+  AppBarComponent,
+  AddDataComponent
+];
+
+export const entryComponents = [AddDataComponent];
+
+export * from './add-data/add-data.component';
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBab6G_FbcWkK8zZBTEBc0Ih7-Nvgu2V8Q',
+    authDomain: 'techsylvania-2019-demo.firebaseapp.com',
+    databaseURL: 'https://techsylvania-2019-demo.firebaseio.com',
+    projectId: 'techsylvania-2019-demo',
+    storageBucket: 'techsylvania-2019-demo.appspot.com',
+    messagingSenderId: '988155867748',
+    appId: '1:988155867748:web:cf1268449f5151fe'
+  }
 };
