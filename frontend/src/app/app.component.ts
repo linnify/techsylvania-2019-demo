658df547e991fd1e5226d02274201d77d219d452
darius.bogdan <darius.bogdan@linnify.com>
2019-06-01 16:25:22 +0300

    add uploadToBigQuert cf

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <app-app-bar></app-app-bar>
+  `,
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'techsylvania';
-}
+export class AppComponent {}
