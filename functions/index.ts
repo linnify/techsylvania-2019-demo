cb2e88f497fb57a6773ae8d43610b81dbd7a3554
darius.bogdan <darius.bogdan@linnify.com>
2019-06-02 17:16:09 +0300

    change order of functions in index.ts

--- functions/index.ts
+++ functions/index.ts
@@ -3,7 +3,7 @@ import { aggregateData } from './src/aggregate-data';
 import { updateFinalData } from './src/update-final-data';
 import { checkAggregateJob } from './src/check-aggregate-job';
 
-exports.updateFinalData = updateFinalData;
-exports.checkAggregateJob = checkAggregateJob;
-exports.aggregateData = aggregateData;
 exports.uploadToBigQuery = uploadToBigQuery;
+exports.aggregateData = aggregateData;
+exports.checkAggregateJob = checkAggregateJob;
+exports.updateFinalData = updateFinalData;
