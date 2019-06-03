caaaec06a4b2d08bb59afcf93491efb9677454fd
darius.bogdan <darius.bogdan@linnify.com>
2019-06-03 19:44:38 +0300

    resolve endpoint error

--- api/src/controllers/api.ts
+++ api/src/controllers/api.ts
@@ -13,22 +13,20 @@ export let health = (req: Request, res: Response) => {
 };
 
 export const getMaxMeanValue = async (req: Request, res: Response) => {
-  console.log('start');
   const query: string = `
     SELECT * FROM (
         SELECT sourceid AS sourceId, dstid AS destinationId, 
         source_name AS sourceName, destination_name AS destinationName, 
         AVG(mean_travel_time) as meanTravelTime 
         FROM \`techsylvania-2019-demo.uber.final_destination\`
-        GROUP BY sourceid, dstid, source_name, destination_name
-        ORDER BY mean_travel_time DESC
+        GROUP BY sourceId, destinationId, sourceName, destinationName
+        ORDER BY meanTravelTime DESC
     )
     LIMIT 1
   `;
 
   const [job] = await bigQueryClient.createQueryJob({ query });
 
-  console.log('end');
   const [rows] = await job.getQueryResults();
 
   const dataRow: DataRow = rows[0];
