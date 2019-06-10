ebecfc4727dcfd5f97ccde4f2ec59dc439bfe57e
razvanbretoiu <razvan.bretoiu@linnify.com>
2019-06-10 11:05:22 +0300

    fix: web merge problems

--- frontend/src/app/components/max-mean-card/max-mean-card.component.ts
+++ frontend/src/app/components/max-mean-card/max-mean-card.component.ts
@@ -5,51 +5,44 @@ import { FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-max-mean-card',
   template: `
-    <<<<<<< HEAD
     <mat-card style="height: 258px;">
       <mat-card-title fxLayout="row" fxLayoutAlign="space-between center">
         <div>Max Average Travel Time</div>
         <button mat-button color="primary" (click)="refresh.emit()">
           <mat-icon>refresh</mat-icon> Refresh
         </button>
-        =======
-        <mat-card style="height: 265px">
-          <mat-card-title>
-            Max Average Travel Time >>>>>>>
-            c1d73db82c813da6658f29158f03dd1361f41b43
-          </mat-card-title>
-          <mat-card-content fxLayout="column">
-            <ng-container *ngIf="data && !loading; else noData">
-              <mat-form-field class="full-width" appearance="outline">
-                <mat-label>Source</mat-label>
-                <input matInput [value]="data.sourceName" disabled />
-                <mat-icon matPrefix>place</mat-icon>
-              </mat-form-field>
-              <mat-form-field class="full-width" appearance="outline">
-                <mat-label>Destination</mat-label>
-                <input matInput [value]="data.destinationName" disabled />
-                <mat-icon matPrefix>place</mat-icon>
-              </mat-form-field>
-              <div class="mat-title">
-                Time: {{ data.meanTravelTime | duration }}
-              </div>
-            </ng-container>
-            <ng-template #noData>
-              <div class="no-data mat-title">
-                No data
-              </div>
-            </ng-template>
-            <div
-              *ngIf="loading"
-              fxLayout="row"
-              fxLayoutAlign="center center"
-              class="padding-24"
-            >
-              <mat-spinner [diameter]="30"></mat-spinner>
-            </div>
-          </mat-card-content>
-        </mat-card> </mat-card-title
-    ></mat-card>
+      </mat-card-title>
+      <mat-card-content fxLayout="column">
+        <ng-container *ngIf="data && !loading; else noData">
+          <mat-form-field class="full-width" appearance="outline">
+            <mat-label>Source</mat-label>
+            <input matInput [value]="data.sourceName" disabled />
+            <mat-icon matPrefix>place</mat-icon>
+          </mat-form-field>
+          <mat-form-field class="full-width" appearance="outline">
+            <mat-label>Destination</mat-label>
+            <input matInput [value]="data.destinationName" disabled />
+            <mat-icon matPrefix>place</mat-icon>
+          </mat-form-field>
+          <div class="mat-title">
+            Time: {{ data.meanTravelTime | duration }}
+          </div>
+        </ng-container>
+        <ng-template #noData>
+          <div class="no-data mat-title">
+            No data
+          </div>
+        </ng-template>
+        <div
+          *ngIf="loading"
+          fxLayout="row"
+          fxLayoutAlign="center center"
+          class="padding-24"
+        >
+          <mat-spinner [diameter]="30"></mat-spinner>
+        </div>
+      </mat-card-content>
+    </mat-card>
   `,
   styleUrls: ['./max-mean-card.component.scss']
 })
